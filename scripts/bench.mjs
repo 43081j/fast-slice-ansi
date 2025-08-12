90aac3180db9c311d35cc02b784333bc9eabc58c
James Garbutt <43081j@users.noreply.github.com>
2025-08-12 16:58:04 +0100

    chore: add multiple benchmark cases (#4)

--- scripts/bench.mjs
+++ scripts/bench.mjs
@@ -3,20 +3,51 @@ import legacySlice from 'slice-ansi';
 import {sliceAnsi} from '../lib/main.js';
 import {styleText} from 'node:util';
 
-const bench = new Bench();
-const input = `Hello, I am ${styleText('blue', 'blue text')}, and
-I am ${styleText('red', 'red text')}.`;
+const inputs = [
+  {
+    name: 'simple',
+    input: `Hello, I am ${styleText('blue', 'blue text')}, and
+      I am ${styleText('red', 'red text')}.`,
+    start: 0,
+    end: 5
+  },
+  {
+    name: 'short',
+    input: `A short ${styleText('green', 'green')} example.`,
+    start: 0,
+    end: 5
+  },
+  {
+    name: 'long',
+    input: `A somewhat long ${styleText('red', ' red text ').repeat(20)} example`,
+    start: 0,
+    end: 5
+  },
+  {
+    name: 'nested colours',
+    input: `Some ${styleText('blue', `nested ${styleText('red', 'colours')}`)} example.`,
+    start: 0,
+    end: 5
+  },
+  {
+    name: 'middle of colour',
+    input: `This is a ${styleText('green', 'green text')} example.`,
+    start: 15,
+    end: undefined
+  }
+];
 
-bench.add('fast-slice-ansi', () => {
-  sliceAnsi(input, 0, 5);
-});
-//bench.add('fast-slice-ansi (main)', () => {
-  //sliceMain(input, 0, 5);
-//});
-bench.add('slice-ansi', () => {
-  legacySlice(input, 0, 5);
-});
+for (const {name, input, start, end} of inputs) {
+  const bench = new Bench();
 
-await bench.run();
+  bench.add(`slice-ansi (${name})`, () => {
+    legacySlice(input, start, end);
+  });
+  bench.add(`fast-slice-ansi (${name})`, () => {
+    sliceAnsi(input, start, end);
+  });
+  console.log(name);
+  await bench.run();
 
-console.table(bench.table());
+  console.table(bench.table());
+}
